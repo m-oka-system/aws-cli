bf50a5a509f506913705cef1a3777a93d81214bf
m-oka-system <m-oka-system@outlook.com>
2020-05-11 22:51:22 +0900

    Update at 20200511

--- rds/rds-create-db-instance.sh
+++ rds/rds-create-db-instance.sh
@@ -15,7 +15,30 @@ for i in ${servers[@]}; do
   dbUserName=$(echo $i | cut -d , -f 10)
   dbUserPassword=$(echo $i | cut -d , -f 11)
   multAz=$(echo $i | cut -d , -f 12)
+
+  # Create subnet group
+  dbSubnetIds="subnet-0d16588ec31769ede subnet-0c31fe13b8264bb70"
+  aws rds create-db-subnet-group \
+    --db-subnet-group-name $dbInstanceIdentifier"-subnet" \
+    --db-subnet-group-description $dbInstanceIdentifier"-subnet" \
+    --subnet-ids $dbSubnetIds \
+    --tags Key=Name,Value=$dbInstanceIdentifier"-subnet" Key=env,Value=dev
   
+  # Create parameter group
+  aws rds create-db-parameter-group \
+    --db-parameter-group-name $dbInstanceIdentifier"-param" \
+    --db-parameter-group-family mysql5.7 \
+    --description $dbInstanceIdentifier"-param" \
+    --tags Key=Name,Value=$dbInstanceIdentifier"-param" Key=env,Value=dev
+
+  # Create option group
+  aws rds create-option-group \
+    --option-group-name $dbInstanceIdentifier"-opt" \
+    --engine-name mysql \
+    --major-engine-version 5.7 \
+    --option-group-description $dbInstanceIdentifier"-opt" \
+    --tags Key=Name,Value=$dbInstanceIdentifier"-opt" Key=env,Value=dev
+
   # Create RDS instances
   # You can't set the AvailabilityZone parameter if the DB instance is a Multi-AZ deployment.
   aws rds create-db-instance \
