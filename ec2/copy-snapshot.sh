62b47d26756ec5cd09502d3673fbcc60f3241eb0
m-oka-system <m-oka-system@outlook.com>
2020-06-15 21:20:59 +0900

    Add copy snapshot

---
+++ ec2/copy-snapshot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+srcRegion="ap-northeast-1"
+dstRegion="ap-southeast-1"
+hostName="WEB01"
+snapshotIds=$(aws ec2 describe-snapshots --filters "Name=tag:Name,Values=$hostName*" --query "reverse(sort_by(Snapshots, &StartTime))[:1].SnapshotId" --output text)
+
+# Copy snapshot to Singapore region
+aws ec2 copy-snapshot --region $dstRegion \
+--destination-region $srcRegion \
+--source-region $dstRegion \
+--source-snapshot-id $snapshotIds \
+--tag-specifications "ResourceType=snapshot,Tags=[{Key=Name,Value=${hostName}},{Key=env,Value=dev}]"
