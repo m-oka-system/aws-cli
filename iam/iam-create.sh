f9c2bfe15331ed7ebb77a819a1edbc3b55d3f721
m-oka-system <m-oka-system@outlook.com>
2020-08-13 21:51:16 +0900

    [update]ホスト名を引数指定として複数作成に対応

--- iam/iam-create.sh
+++ iam/iam-create.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-HostName=WEB01
+servers="$@"
 
-# Create IAM role
-aws iam create-role \
-  --role-name $HostName"_IAMROLE" \
-  --assume-role-policy-document file://iamrole.json
+if [ "$#" -eq 0 ]; then
+  echo  "1つ以上の引数を指定してください。"
+  exit 1
+fi
 
-# Create IAM policy
-IAMPOLICY_ARN=$(aws iam create-policy \
-  --policy-name $HostName"_IAMPOLICY" \
-  --policy-document file://iampolicy.json \
-  --output text --query "Policy.Arn")
+for i in ${servers[@]}; do
+  HostName=$i
 
-# Attach policy to IAM role
-aws iam attach-role-policy \
-  --policy-arn $IAMPOLICY_ARN \
-  --role-name $HostName"_IAMROLE"
+  # Create IAM role
+  aws iam create-role \
+    --role-name $HostName"_IAMROLE" \
+    --assume-role-policy-document file://iamrole.json
 
-# Create instance profile
-aws iam create-instance-profile \
-  --instance-profile-name $HostName"_IAMROLE"
+  # Create IAM policy
+  IAMPOLICY_ARN=$(aws iam create-policy \
+    --policy-name $HostName"_IAMPOLICY" \
+    --policy-document file://iampolicy.json \
+    --output text --query "Policy.Arn")
 
-# Add IAM role to instance profile
-aws iam add-role-to-instance-profile \
-  --role-name $HostName"_IAMROLE" \
-  --instance-profile-name $HostName"_IAMROLE"
+  # Attach policy to IAM role
+  aws iam attach-role-policy \
+    --policy-arn $IAMPOLICY_ARN \
+    --role-name $HostName"_IAMROLE"
+
+  # Create instance profile
+  aws iam create-instance-profile \
+    --instance-profile-name $HostName"_IAMROLE"
+
+  # Add IAM role to instance profile
+  aws iam add-role-to-instance-profile \
+    --role-name $HostName"_IAMROLE" \
+    --instance-profile-name $HostName"_IAMROLE"
+done
