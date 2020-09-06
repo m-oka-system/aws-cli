c31ca1f73610ffd79f850cab65d7f928bcc5bdb8
m-oka-system <m-oka-system@outlook.com>
2020-09-06 21:45:20 +0900

    [fix]shellcheckの問題点を修正

--- vpc/create-vpc.sh
+++ vpc/create-vpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Common Variables
-REGION="ap-northeast-1"
+export AWS_DEFAULT_REGION="ap-northeast-1"
 
 # VPC
 VPC_NAME="awscli-vpc"
@@ -13,8 +13,8 @@ PUBLIC_SUBNET_CIDR_BLOCKS=("10.100.11.0/24" "10.100.12.0/24")
 PUBLIC_SUBNET_AZ=("${REGION}a" "${REGION}c")
 
 # Create vpc
-VPC_ID=$(aws ec2 create-vpc --region $REGION \
-  --cidr-block $VPC_CIDR_BLOCK \
+VPC_ID=$(aws ec2 create-vpc \
+  --cidr-block "$VPC_CIDR_BLOCK" \
   --instance-tenancy default \
   --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=$VPC_NAME},{Key=Env,Value=Dev}]" \
   --query "Vpc.VpcId" --output text)
@@ -22,44 +22,44 @@ VPC_ID=$(aws ec2 create-vpc --region $REGION \
 # Create public subnet
 declare -a PUBLIC_SUBNET_IDS=()
 for (( i=0; i < ${#PUBLIC_SUBNET_NAMES[*]}; i++ )); do
-  PUBLIC_SUBNET_ID=$(aws ec2  create-subnet --region $REGION \
-  --vpc-id $VPC_ID \
-  --cidr-block ${PUBLIC_SUBNET_CIDR_BLOCKS[$i]} \
-  --availability-zone ${PUBLIC_SUBNET_AZ[$i]} \
+  PUBLIC_SUBNET_ID=$(aws ec2  create-subnet \
+  --vpc-id "$VPC_ID" \
+  --cidr-block "${PUBLIC_SUBNET_CIDR_BLOCKS[$i]}" \
+  --availability-zone "${PUBLIC_SUBNET_AZ[$i]}" \
   --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=${PUBLIC_SUBNET_NAMES[$i]}},{Key=Env,Value=Dev}]" \
   --query "Subnet.SubnetId" --output text)
-  PUBLIC_SUBNET_IDS+=($PUBLIC_SUBNET_ID)
+  PUBLIC_SUBNET_IDS+=("$PUBLIC_SUBNET_ID")
 done
 
 # Create internet gateway
-INTERNET_GATEWAY_ID=$(aws ec2 create-internet-gateway --region $REGION \
+INTERNET_GATEWAY_ID=$(aws ec2 create-internet-gateway \
   --tag-specifications "ResourceType=internet-gateway,Tags=[{Key=Name,Value=${VPC_NAME}-igw},{Key=Env,Value=Dev}]" \
   --query "InternetGateway.InternetGatewayId" --output text)
 
 # Attach internet gateway to vpc
-aws ec2 attach-internet-gateway --region $REGION --internet-gateway-id $INTERNET_GATEWAY_ID --vpc-id $VPC_ID
+aws ec2 attach-internet-gateway --internet-gateway-id "$INTERNET_GATEWAY_ID" --vpc-id "$VPC_ID"
 
 # Create route table for public subnet
 declare -a ROUTE_TABLE_IDS=()
 for (( i=0; i < ${#PUBLIC_SUBNET_NAMES[*]}; i++ )); do
-  ROUTE_TABLE_ID=$(aws ec2 create-route-table --region $REGION \
-    --vpc-id $VPC_ID \
+  ROUTE_TABLE_ID=$(aws ec2 create-route-table \
+    --vpc-id "$VPC_ID" \
     --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=${PUBLIC_SUBNET_NAMES[$i]}-rt},{Key=Env,Value=Dev}]" \
     --query "RouteTable.RouteTableId" --output text)
-    ROUTE_TABLE_IDS+=($ROUTE_TABLE_ID)
+    ROUTE_TABLE_IDS+=("$ROUTE_TABLE_ID")
 done
 
 # Create route in route table
 for (( i=0; i < ${#ROUTE_TABLE_IDS[*]}; i++ )); do
-  aws ec2 create-route --region $REGION \
-    --route-table-id ${ROUTE_TABLE_IDS[$i]} \
+  aws ec2 create-route \
+    --route-table-id "${ROUTE_TABLE_IDS[$i]}" \
     --destination-cidr-block 0.0.0.0/0 \
-    --gateway-id $INTERNET_GATEWAY_ID
+    --gateway-id "$INTERNET_GATEWAY_ID"
 done
 
 # Associate route table with subnet
 for (( i=0; i < ${#ROUTE_TABLE_IDS[*]}; i++ )); do
-  aws ec2 associate-route-table --region $REGION \
-    --route-table-id ${ROUTE_TABLE_IDS[$i]} \
-    --subnet-id ${PUBLIC_SUBNET_IDS[$i]}
+  aws ec2 associate-route-table \
+    --route-table-id "${ROUTE_TABLE_IDS[$i]}" \
+    --subnet-id "${PUBLIC_SUBNET_IDS[$i]}"
 done
--- vpc/delete-vpc.sh
+++ vpc/delete-vpc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Variables
-REGION="ap-northeast-1"
+export AWS_DEFAULT_REGION="ap-northeast-1"
 VPC_NAME="awscli-vpc"
 PUBLIC_SUBNET_NAMES=("awscli-public-subnet-1a" "awscli-public-subnet-1c")
 
 # Delete public subnet
 for (( i=0; i < ${#PUBLIC_SUBNET_NAMES[*]}; i++ )); do
-  SUBNET_ID=$(aws ec2 describe-subnets --region $REGION --filters "Name=tag:Name,Values=${PUBLIC_SUBNET_NAMES[$i]}" --query "Subnets[].SubnetId" --output text)
-  aws ec2 delete-subnet --region $REGION --subnet-id $SUBNET_ID
+  SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=${PUBLIC_SUBNET_NAMES[$i]}" --query "Subnets[].SubnetId" --output text)
+  aws ec2 delete-subnet --subnet-id "$SUBNET_ID"
 done
 
 # Delete vpc
-VPC_ID=$(aws ec2 describe-vpcs --region $REGION --filters "Name=tag:Name,Values=${VPC_NAME}" --query "Vpcs[].VpcId" --output text)
-aws ec2 delete-vpc --region $REGION --vpc-id $VPC_ID
+VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=${VPC_NAME}" --query "Vpcs[].VpcId" --output text)
+aws ec2 delete-vpc --vpc-id "$VPC_ID"
