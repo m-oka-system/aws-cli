31c257e089d701f34d4f9bf7909e8a9babd6b796
m-oka-system <m-oka-system@outlook.com>
2020-09-19 16:05:33 +0900

    [add]cli53を利用したexport/importのスクリプトを追加

---
+++ route53/cli53/swichback-record-sets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# 変数
+export AWS_DEFAULT_REGION="ap-northeast-1"
+DOMAIN_NAME="example.local"
+ZONE_ID="$@"
+
+if [ "$#" -ne 1 ]; then
+  echo  "ZoneIDを引数に指定してください。"
+  exit 1
+fi
+
+# ホストゾーンに変更前のファイルをインポート
+cli53 import --file ${AWS_DEFAULT_REGION}_${DOMAIN_NAME}_${ZONE_ID}_before.txt --replace $ZONE_ID
