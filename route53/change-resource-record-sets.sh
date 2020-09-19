3f7e2016f70a0609913d69c1ff43fb83986f8b74
m-oka-system <m-oka-system@outlook.com>
2020-09-19 14:40:49 +0900

    [add]リソースレコードを更新するスクリプトを追加

---
+++ route53/change-resource-record-sets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# 共通
+export AWS_DEFAULT_REGION="ap-northeast-1"
+DOMAIN_NAME="example.local"
+VPC_ID="vpc-08ad5c00c7cdece78"
+ZONE_ID=$(aws route53 list-hosted-zones-by-vpc --vpc-id $VPC_ID --vpc-region $AWS_DEFAULT_REGION --query "HostedZoneSummaries[?Name==\`${DOMAIN_NAME}.\`].HostedZoneId" --output text)
+
+# リソースレコードを更新
+echo "リソースレコードを更新します。"
+aws route53 change-resource-record-sets \
+--hosted-zone-id $ZONE_ID \
+--change-batch file://recordsets/${AWS_DEFAULT_REGION}_${DOMAIN_NAME}.json 2>&1 1>/dev/null
+echo "リソースレコードを更新しました。"
