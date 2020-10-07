ca21c2258d178642f5489cfdc5e07a91e66a105e
m-oka-system <m-oka-system@outlook.com>
2020-10-07 09:49:41 +0900

    [clean]EFSリストアのスクリプトをフォルダ移動

---
+++ aws-backup/efs/create-mount-target.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -euo pipefail
+
+# 変数
+SERVER_FILE="efs-list.csv"
+CREATED_EFS_FILE="CreatedEfsList"
+
+# ファイルを1行ずつ読み込んで配列へ格納
+mapfile -t SERVER_ARRAY < <(sed 1d $SERVER_FILE | sed '/^#/d')
+mapfile -t CREATED_EFS < <(cat $CREATED_EFS_FILE)
+
+echo "${SERVER_ARRAY[@]}"
+read -p "EFSにマウントターゲットを追加します。よろしいですか？ (y/N): " yn
+case "$yn" in [yY]*) ;; *) echo "処理を終了します." ; exit ;; esac
+
+# Main
+n=0
+for i in "${SERVER_ARRAY[@]}"; do
+  # Variables
+  REGION=$(echo $i | cut -d , -f 1)
+  EFS_NAME=$(echo $i | cut -d , -f 2)
+  SUBNET_ID1=$(echo $i | cut -d , -f 4)
+  SUBNET_ID2=$(echo $i | cut -d , -f 5)
+  PRIVATE_IP1=$(echo $i | cut -d , -f 6)
+  PRIVATE_IP2=$(echo $i | cut -d , -f 7)
+  SECURITY_GROUP_ID=$(echo $i | cut -d , -f 8)
+  LATEST_RECOVERY_POINT=$(echo ${CREATED_EFS[$n]} | cut -d , -f 2)
+  RESTORE_JOB_ID=$(echo ${CREATED_EFS[$n]} | cut -d , -f 3)
+  CREATED_EFS_NAME=${EFS_NAME}-restored
+  SUBNET_IDS=("$SUBNET_ID1" "$SUBNET_ID2")
+  EFS_IP_ADDRESSES=("$PRIVATE_IP1" "$PRIVATE_IP2")
+
+  echo "$EFS_NAME のマウントターゲット作成を開始します。"
+  # リストアしたEFSのIDを取得
+  CREATED_EFS_ID=$(aws backup describe-restore-job --region $REGION --restore-job-id $RESTORE_JOB_ID --query CreatedResourceArn --output text | awk -F/ '{print $2}')
+  if [ x = x$CREATED_EFS_ID ]; then
+    echo "${EFS_NAME} のEFSID取得に失敗しました。リストア処理が完了していません。処理をスキップします。"
+    n=$((n + 1))
+    continue
+  fi
+
+  # マウントターゲットが存在する場合は処理を終了
+  MOUNT_TARGETS=$(aws efs describe-mount-targets --region $REGION --file-system-id $CREATED_EFS_ID --query "length(MountTargets)" --output text)
+  if [ $MOUNT_TARGETS -gt 0 ]; then
+    echo "${EFS_NAME} のマウントターゲットはすでに登録されています。処理をスキップします。"
+    n=$((n + 1))
+    continue
+  fi
+
+  # マウントターゲットを作成(ENIのIDは配列へ格納)
+  declare -a NETWORK_INTERFACE_IDS=()
+  for ((j=0; j < ${#SUBNET_IDS[*]}; j++)); do
+    NETWORK_INTERFACE_ID=$(aws efs create-mount-target --region $REGION --file-system-id $CREATED_EFS_ID --subnet-id ${SUBNET_IDS[$j]} --ip-address ${EFS_IP_ADDRESSES[$j]} --security-groups $SECURITY_GROUP_ID --query NetworkInterfaceId --output text)
+    NETWORK_INTERFACE_IDS[$j]=$NETWORK_INTERFACE_ID
+  done
+
+  # 最新の復旧ポイントのタグを取得
+  echo "---最新の復旧ポイントのタグをEFS、ENIにコピーします。"
+  # LATEST_RECOVERY_POINT_TAGS=($(aws backup list-tags --region $REGION --resource-arn $LATEST_RECOVERY_POINT --query Tags --output yaml | sed -e's/ //g'))
+  mapfile -t LATEST_RECOVERY_POINT_TAGS < <(aws backup list-tags --region $REGION --resource-arn $LATEST_RECOVERY_POINT --query "Tags" --output yaml | sed -e 's/ //g')
+  for k in "${LATEST_RECOVERY_POINT_TAGS[@]}"; do
+    KEY=$(echo $k | cut -d : -f 1)
+    VALUE=$(echo $k | cut -d : -f 2)
+    aws efs tag-resource --region $REGION --resource-id $CREATED_EFS_ID --tags Key=$KEY,Value=$VALUE
+    aws ec2 create-tags --region $REGION --resources "${NETWORK_INTERFACE_IDS[@]}" --tags Key=$KEY,Value=$VALUE
+  done
+
+  # Nameタグを上書き
+  echo "---EFSとENIのNameタグを上書きします。"
+  aws efs tag-resource --region $REGION --resource-id $CREATED_EFS_ID --tags Key=Name,Value=$CREATED_EFS_NAME
+  aws ec2 create-tags --region $REGION --resources "${NETWORK_INTERFACE_IDS[@]}" --tags Key=Name,Value=${CREATED_EFS_NAME}_ENI
+
+  n=$((n + 1))
+  echo "$EFS_NAME のマウントターゲット作成が終了しました。"
+done
---
+++ aws-backup/efs/start-restore-job-efs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -euo pipefail
+
+# 変数
+SERVER_FILE="efs-list.csv"
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+IAMROLE_ARN="arn:aws:iam::$ACCOUNT_ID:role/service-role/AWSBackupDefaultServiceRole"
+SOURCE_REGION="ap-northeast-1"
+TOKEN=$(uuidgen)
+
+# サーバ一覧のファイルを1行ずつ読み込んで配列へ格納
+mapfile -t SERVER_ARRAY < <(sed 1d $SERVER_FILE | sed '/^#/d')
+
+echo "${SERVER_ARRAY[@]}"
+read -p "EFSのリストアを開始します。よろしいですか？ (y/N): " yn
+case "$yn" in [yY]*) ;; *) echo "処理を終了します." ; exit ;; esac
+
+# リストア済みEFSリストを初期化
+: > CreatedEfsList
+
+# メイン処理
+for i in "${SERVER_ARRAY[@]}"; do
+  # Variables
+  REGION=$(echo $i | cut -d , -f 1)
+  EFS_NAME=$(echo $i | cut -d , -f 2)
+  VAULT_NAME=$(echo $i | cut -d , -f 3)
+
+  echo "$EFS_NAME のリストアを開始しました。"
+  # Get efs id from source region
+  SOURCE_EFS_ID=$(aws efs describe-file-systems --region $SOURCE_REGION --query "FileSystems[?Name==\`${EFS_NAME}\`].FileSystemId" --output text)
+  if [ x = x$SOURCE_EFS_ID ]; then
+    echo "EFSIDの取得に失敗しました。処理を終了します。"
+    exit 1
+  fi
+
+  # AWSBackupの最新の復旧ポイントからEFSをリストア
+  LATEST_RECOVERY_POINT=$(aws backup list-recovery-points-by-backup-vault --region $REGION --backup-vault-name $VAULT_NAME --query "sort_by(RecoveryPoints, &CreationDate)[?contains(ResourceArn,\`$SOURCE_EFS_ID\`)]|[-1].RecoveryPointArn" --output text)
+  if [ x = x$LATEST_RECOVERY_POINT ]; then
+    echo "復旧ポイントの取得に失敗しました。処理を終了します。"
+    exit 1
+  fi
+
+  echo "LatestRecoveryPointArn:${LATEST_RECOVERY_POINT}"
+  read -p "上記復旧ポイントからリストアします。よろしいですか？ (y/N): " yn
+  case "$yn" in [yY]*) ;; *) echo "処理を終了します." ; exit ;; esac
+
+  RESTORE_JOB_ID=$(aws backup start-restore-job --region $REGION \
+    --recovery-point-arn $LATEST_RECOVERY_POINT \
+    --iam-role-arn $IAMROLE_ARN \
+    --resource-type EFS \
+    --metadata file-system-id=$SOURCE_EFS_ID,newFileSystem=true,CreationToken=$TOKEN,Encrypted=false,PerformanceMode=generalPurpose \
+    --output text)
+
+  echo "${EFS_NAME},${LATEST_RECOVERY_POINT},${RESTORE_JOB_ID}" >> CreatedEfsList
+  echo "---$EFS_NAME restore finished---"
+done
+
+echo "EFSリストアのステータスはコンソール画面から確認してください。"
+echo "EFSリストア完了後、マウントターゲットの追加を行ってください。"
