a038b9699a28d3c9760ea096b582b238df5b09e1
m-oka-system <m-oka-system@outlook.com>
2020-12-12 16:09:40 +0900

    [update]EC2作成/リストアの共通処理を関数化

--- ec2/create-ec2.sh
+++ ec2/create-ec2.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 
 # 変数
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SERVER_FILE="server-list.csv"
-IAMROLE_PREFIX="IAMROLE_"
+IAM_INSTANCE_PROFILE="IAMROLE_EC2_INSTANCES"
 TENANCY="default" # "default"|"dedicated"|"host"
 KEY_NAME="my-key-pair"
 
 # サーバ一覧のファイルを1行ずつ読み込んで配列へ格納
-mapfile -t SERVER_ARRAY < <(sed 1d $SERVER_FILE | sed '/^#/d')
+mapfile -t SERVER_ARRAY < <(sed 1d ${SCRIPT_DIR}/$SERVER_FILE | sed '/^#/d')
 
 # 確認メッセージ
 echo "Region:${AWS_DEFAULT_REGION}"
@@ -17,54 +18,57 @@ read -r -p "上記サーバを作成します。よろしいですか？ (y/N): 
 case "$yn" in [yY]*) ;; *) echo "処理を終了します." ; exit ;; esac
 
 # メイン処理
-for server in "${SERVER_ARRAY[@]}"; do
-  # Variables
-  AMI_ID=$(echo $server | cut -d , -f 1)
-  HOST_NAME=$(echo $server | cut -d , -f 2)
-  ENVIRONMENT=$(echo $server | cut -d , -f 3)
-  OS=$(echo $server | cut -d , -f 4)
-  INSTANCE_TYPE=$(echo $server | cut -d , -f 5)
-  PRIVATE_IP=$(echo $server | cut -d , -f 6)
-  SUBNET_ID=$(echo $server | cut -d , -f 7)
-  SECURITY_GROUP_ID=$(echo $server | cut -d , -f 8)
-  # test $OS == "Linux" && USER_DATA="init-linux.sh" || USER_DATA="init-windows.ps1"
-  case $OS in
-    RHEL|CentOS) USER_DATA="init-rhel.sh" ;;
-    Amazon) USER_DATA="init-amazon.sh" ;;
-    Windows) USER_DATA="init-windows.ps1" ;;
-  esac
+function main() {
 
-  # EC2インスタンスを作成
-  echo "${HOST_NAME} の作成を開始しました。"
+  # . ${SCRIPT_DIR}/lib/common
+  . ${SCRIPT_DIR}/lib/ec2
 
-  INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --count 1 \
-    --instance-type $INSTANCE_TYPE \
-    --key-name $KEY_NAME \
-    --subnet-id $SUBNET_ID \
-    --private-ip-address $PRIVATE_IP \
-    --security-group-ids $SECURITY_GROUP_ID \
-    --block-device-mappings file://volume/${HOST_NAME}.json \
-    --iam-instance-profile Name=${IAMROLE_PREFIX}${HOST_NAME} \
-    --user-data file://userdata/${USER_DATA} \
-    --disable-api-termination \
-    --placement Tenancy=${TENANCY} \
-    --capacity-reservation-specification CapacityReservationPreference=none \
-    --query "Instances[].InstanceId" --output text)
+  for server in "${SERVER_ARRAY[@]}"; do
+    # Variables
+    AMI_ID=$(echo $server | cut -d , -f 1)
+    HOST_NAME=$(echo $server | cut -d , -f 2)
+    ENVIRONMENT=$(echo $server | cut -d , -f 3)
+    OS=$(echo $server | cut -d , -f 4)
+    INSTANCE_TYPE=$(echo $server | cut -d , -f 5)
+    PRIVATE_IP=$(echo $server | cut -d , -f 6)
+    SUBNET_ID=$(echo $server | cut -d , -f 7)
+    SECURITY_GROUP_ID=$(echo $server | cut -d , -f 8)
 
-  # タグ付与
-  sleep 1
-  ROOT_VOLUME_ID=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId --output text)
-  VOLUME_IDS=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query Reservations[0].Instances[0].BlockDeviceMappings[].Ebs.VolumeId --output text)
-  NETWORK_INTERFACE_IDS=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query Reservations[0].Instances[0].NetworkInterfaces[].NetworkInterfaceId --output text)
+    case $OS in
+      RHEL*|CentOS*) USER_DATA="init-rhel.sh" ;;
+      Amazon*) USER_DATA="init-amazon.sh" ;;
+      Windows*) USER_DATA="init-windows.ps1" ;;
+    esac
 
-  {
-  aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$HOST_NAME Key=Env,Value=$ENVIRONMENT Key=OS,Value=$OS
-  aws ec2 create-tags --resources $VOLUME_IDS --tags Key=Name,Value=${HOST_NAME}_DATA
-  aws ec2 create-tags --resources $ROOT_VOLUME_ID --tags Key=Name,Value=${HOST_NAME}_ROOT
-  aws ec2 create-tags --resources $NETWORK_INTERFACE_IDS --tags Key=Name,Value=${HOST_NAME}_ENI
-  } 2>&1 1>/dev/null
+    # ネットワークインターフェイスIDを取得
+    NETWORK_INTERFACE_ID=$(get_network_interface_id_by_private_ip)
 
-  echo "${HOST_NAME} の作成が終了しました。"
-done
+    # ネットワークインターフェイスが存在しなければ新規作成
+    if [ -z "$NETWORK_INTERFACE_ID" ]; then
+      NETWORK_INTERFACE_ID=$(create_network_interface)
+      aws ec2 create-tags --resources $NETWORK_INTERFACE_ID --tags Key=Name,Value=${HOST_NAME}_ENI
+    fi
+
+    # EC2インスタンスを作成
+    echo "${HOST_NAME} の作成を開始しました。"
+
+    INSTANCE_ID=$(create_ec2)
+
+    # タグ付与
+    sleep 1
+    ROOT_VOLUME_ID=$(get_root_volume_id)
+    VOLUME_IDS=$(get_volume_ids)
+
+    {
+    aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$HOST_NAME Key=Env,Value=$ENVIRONMENT Key=OS,Value=$OS
+    aws ec2 create-tags --resources $VOLUME_IDS --tags Key=Name,Value=${HOST_NAME}_DATA
+    aws ec2 create-tags --resources $ROOT_VOLUME_ID --tags Key=Name,Value=${HOST_NAME}_ROOT
+    } 2>&1 1>/dev/null
+
+    echo "${HOST_NAME} の作成が終了しました。"
+  done
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
--- ec2/restore-ec2.sh
+++ ec2/restore-ec2.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 
 # 変数
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SERVER_FILE="restore-list.csv"
-IAMROLE_PREFIX="IAMROLE_"
+IAM_INSTANCE_PROFILE="IAMROLE_EC2_INSTANCES"
 TENANCY="default" # "default"|"dedicated"|"host"
 KEY_NAME="my-key-pair"
 
 # サーバ一覧のファイルを1行ずつ読み込んで配列へ格納
-mapfile -t SERVER_ARRAY < <(sed 1d $SERVER_FILE | sed '/^#/d')
+mapfile -t SERVER_ARRAY < <(sed 1d ${SCRIPT_DIR}/$SERVER_FILE | sed '/^#/d')
 
 # 確認メッセージ
 echo "Region:${AWS_DEFAULT_REGION}"
@@ -17,55 +18,61 @@ read -r -p "上記サーバを作成します。よろしいですか？ (y/N): 
 case "$yn" in [yY]*) ;; *) echo "処理を終了します." ; exit ;; esac
 
 # メイン処理
-for server in "${SERVER_ARRAY[@]}"; do
-  # Variables
-  AMI_ID=$(echo $server | cut -d , -f 1)
-  HOST_NAME=$(echo $server | cut -d , -f 2)
-  ENVIRONMENT=$(echo $server | cut -d , -f 3)
-  OS=$(echo $server | cut -d , -f 4)
-  INSTANCE_TYPE=$(echo $server | cut -d , -f 5)
-  PRIVATE_IP=$(echo $server | cut -d , -f 6)
+function main() {
 
-  case $OS in
-    RHEL) USER_DATA="dr-rhel.sh" ;;
-    Amazon) USER_DATA="init-amazon.sh" ;;
-    Windows) USER_DATA="dr-windows.ps1" ;;
-  esac
+  # . ${SCRIPT_DIR}/lib/common
+  . ${SCRIPT_DIR}/lib/ec2
 
-  # 最新のAMIを取得
-  LATEST_AMI_ID=$(aws ec2 describe-images --owner self --filter "Name=tag:Name,Values=$HOST_NAME*" --query "sort_by(Images, &CreationDate)[-1].ImageId" --output text)
+  for server in "${SERVER_ARRAY[@]}"; do
+    # Variables
+    HOST_NAME=$(echo $server | cut -d , -f 1)
+    ENVIRONMENT=$(echo $server | cut -d , -f 2)
+    OS=$(echo $server | cut -d , -f 3)
+    INSTANCE_TYPE=$(echo $server | cut -d , -f 4)
+    PRIVATE_IP=$(echo $server | cut -d , -f 5)
 
-  # ネットワークインターフェイスIDを取得
-  NETWORK_INTERFACE_ID=$(aws ec2 describe-network-interfaces --filters Name=addresses.private-ip-address,Values=$PRIVATE_IP --query "NetworkInterfaces[].NetworkInterfaceId" --output text)
+    case $OS in
+      RHEL) USER_DATA="dr-rhel.sh" ;;
+      Amazon) USER_DATA="init-amazon.sh" ;;
+      Windows) USER_DATA="dr-windows.ps1" ;;
+    esac
 
-  # EC2インスタンスを作成
-  echo "${HOST_NAME} の作成を開始しました。"
+    # 最新のAMIを取得
+    LATEST_AMI_ID=$(get_latest_ami)
+    if [ -z "$LATEST_AMI_ID" ]; then
+      echo "AMIIDの取得に失敗しました。処理を終了します。"
+      exit 1
+    fi
 
-  INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id $LATEST_AMI_ID \
-    --count 1 \
-    --instance-type $INSTANCE_TYPE \
-    --key-name $KEY_NAME \
-    --network-interfaces NetworkInterfaceId=${NETWORK_INTERFACE_ID},DeviceIndex=0 \
-    --iam-instance-profile Name=${IAMROLE_PREFIX}${HOST_NAME} \
-    --user-data fileb://userdata/${USER_DATA} \
-    --disable-api-termination \
-    --placement Tenancy=${TENANCY} \
-    --capacity-reservation-specification CapacityReservationPreference=none \
-    --query "Instances[].InstanceId" --output text)
+    # ネットワークインターフェイスIDを取得
+    NETWORK_INTERFACE_ID=$(get_network_interface_id_by_private_ip)
+    if [ -z "$NETWORK_INTERFACE_ID" ]; then
+      echo "ネットワークインターフェイスIDの取得に失敗しました。処理を終了します。"
+      exit 1
+    fi
 
-  # タグ付与
-  sleep 1
-  ROOT_VOLUME_ID=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId --output text)
-  VOLUME_IDS=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query Reservations[0].Instances[0].BlockDeviceMappings[].Ebs.VolumeId --output text)
-  NETWORK_INTERFACE_IDS=$(aws ec2 describe-instances --instance-id $INSTANCE_ID --query Reservations[0].Instances[0].NetworkInterfaces[].NetworkInterfaceId --output text)
+    # EC2インスタンスを作成
+    echo "${HOST_NAME} の作成を開始しました。"
 
-  {
-  aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$HOST_NAME Key=Env,Value=$ENVIRONMENT
-  aws ec2 create-tags --resources $VOLUME_IDS --tags Key=Name,Value=${HOST_NAME}_DATA
-  aws ec2 create-tags --resources $ROOT_VOLUME_ID --tags Key=Name,Value=${HOST_NAME}_ROOT
-  aws ec2 create-tags --resources $NETWORK_INTERFACE_IDS --tags Key=Name,Value=${HOST_NAME}_ENI
-  } 2>&1 1>/dev/null
+    INSTANCE_ID=$(restore_ec2)
 
-  echo "${HOST_NAME} の作成が終了しました。"
-done
+    # タグ付与
+    sleep 1
+    ROOT_VOLUME_ID=$(get_root_volume_id)
+    VOLUME_IDS=$(get_volume_ids)
+    NETWORK_INTERFACE_IDS=$(get_network_interface_ids)
+
+    {
+    aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=$HOST_NAME Key=Env,Value=$ENVIRONMENT
+    aws ec2 create-tags --resources $VOLUME_IDS --tags Key=Name,Value=${HOST_NAME}_DATA
+    aws ec2 create-tags --resources $ROOT_VOLUME_ID --tags Key=Name,Value=${HOST_NAME}_ROOT
+    aws ec2 create-tags --resources $NETWORK_INTERFACE_IDS --tags Key=Name,Value=${HOST_NAME}_ENI
+    } 2>&1 1>/dev/null
+
+    echo "${HOST_NAME} の作成が終了しました。"
+  done
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
